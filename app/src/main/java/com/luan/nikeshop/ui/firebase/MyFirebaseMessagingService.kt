d3d194b1b9039bf04634861115652a9468a1d523
Luanlmr <luanlmr98@gmail.com>
2025-05-29 20:22:12 -0300

    resolvido conflitos de merge

--- app/src/main/java/com/luan/nikeshop/ui/firebase/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/luan/nikeshop/ui/firebase/MyFirebaseMessagingService.kt
@@ -1,4 +1,4 @@
-package com.jailton.androidapptemplate.ui.firebase
+package com.luan.nikeshop.ui.firebase
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
@@ -11,8 +11,8 @@ import android.widget.RemoteViews
 import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
-import com.jailton.androidapptemplate.MainActivity
-import com.jailton.androidapptemplate.R
+import com.luan.nikeshop.MainActivity
+import com.luan.nikeshop.R
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
 
